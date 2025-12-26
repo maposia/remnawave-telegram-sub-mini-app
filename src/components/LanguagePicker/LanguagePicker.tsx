7eeaa3ef8dca9e30b978c6d530f920234b12c43b
Vitalii Miagkov <maposia@gmail.com>
2025-12-26 20:58:44 +0300

    fix: add scrollbar to language selection dropdown

--- src/components/LanguagePicker/LanguagePicker.tsx
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -69,7 +69,9 @@ export function LanguagePicker(props: IProps) {
                     <IconLanguage size={22} />
                 </ActionIcon>
             </Menu.Target>
-            <Menu.Dropdown>{items}</Menu.Dropdown>
+            <Menu.Dropdown mah={250} style={{ overflowY: 'auto' }}>
+                {items}
+            </Menu.Dropdown>
         </Menu>
     )
 }
